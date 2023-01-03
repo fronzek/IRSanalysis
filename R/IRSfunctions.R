0b19a2f05922a822011ad35f610035dae71c91d0
Stefan Fronzek <43381251+fronzek@users.noreply.github.com>
2023-01-03 15:16:08 +0200

    Update IRSfunctions.R

--- R/IRSfunctions.R
+++ R/IRSfunctions.R
@@ -109,9 +109,9 @@ matrix.poly <- function(x, y, z=mat, n=NULL){
 #' @export
 plotIRS <- function(dat, xvar="deltaT", yvar="Pchange", xlab=expression(paste(plain("Temperature change ("),degree,plain("C)"))), ylab="Precipitation change (%)", var="grain_DM", add=FALSE, col="black", lty=1, lwd=1, title=NA, levels=NULL, cex=1, labcex=0.6, drawlabels=TRUE, colSc="normal", xlim=NULL, ylim=NULL, xaxt="s", yaxt="s", legend=FALSE, contours=TRUE, stippled=TRUE) {
 	#require(data.table)
-	print("Test6")
+	print("Test7")
 	# create a local copy of the data with common column names
-	datlocal <- data.table(cbind(dat[, match(xvar,names(dat)),with=FALSE],
+	datlocal <- data.table::data.table(cbind(dat[, match(xvar,names(dat)),with=FALSE],
 		dat[, match(yvar,names(dat)),with=FALSE],
 		dat[, match(var,names(dat)),with=FALSE]))
 	colnames(datlocal) <- c("xvar","yvar","var")
